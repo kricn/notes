ad8eebd4bac09510b10b93c44c254f74a75a3765
陈凯旋 <dev_ckx@mtm.ink>
2021-06-01 14:27:58 +0800

    update

--- js/虚拟列表/code/src/main.js
+++ js/虚拟列表/code/src/main.js
@@ -14,9 +14,34 @@ const getData = (count=100) => {
 
 const data = getData()
 
-const item = document.querySelector('.item')
-const phantom = document.querySelector('.phontom')
+let start = 0
+let end = 0
+let count = 0
+let showData = []
+
+const items = document.querySelectorAll('.item')
+const phantom = document.querySelector('.phantom')
+const list = document.querySelector('.list')
 const container = document.querySelector('.container')
+const size = 60
+const listHeight = Math.ceil(data.length * size)
+
+phantom.style.height = listHeight + 'px'
+items.forEach(item => item.style.height = size + 'px')
+count = Math.ceil(container.offsetHeight / size)
+showData = data.slice(start, Math.min(end, data.length))
+
+container.addEventListener('scroll', () => {
+  let scrollTop = container.scrollTop
+  start = Math.floor(scrollTop / size)
+  end = start + count
+  showData = data.slice(start, Math.min(end, data.length))
+  let offsetTop = scrollTop - (scrollTop % size)
+  list.style.transform = `translate3d(0, ${offsetTop}px, 0)`
+  for (let i = 0; i < showData.length; i ++) {
+    items[i].innerHTML = showData[i].label
+  }
+})
 
 
 
