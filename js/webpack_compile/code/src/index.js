846fdbf714a0b28eb542771f2c45950a15434cea
陈凯旋 <chenkaixuan@doctortech.com.cn>
2021-07-23 15:46:32 +0800

    init webpack compile

---
+++ js/webpack_compile/code/src/index.js
@@ -0,0 +1,4 @@
+import { name } from './modules/module1'
+import { url } from './modules/module2'
+
+console.log(`${name}的github主页是：${url}`)
---
+++ js/webpack_compile/code/src/modules/module2.js
@@ -0,0 +1,3 @@
+console.log('hello world')
+
+export const url = 'https://github.com/kricn'
